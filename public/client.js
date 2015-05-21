5ab8a3b1b46f424b19b2f7fa2fca148d4ae119d2
Peter Gyory <ptg3662@rit.edu>
2015-05-20 20:22:10 -0400

    Fix jade, setup css

--- public/client.js
+++ public/client.js
@@ -1,8 +1,18 @@
 var socket;
 socket = io.connect( window.location.origin, {query: 'bar', type: 'desktop'});
+var ordersDiv = document.querSelector('#orders');
 
-socket.on('new', data{
+socket.on('new', function(data){
 	var order = "";
-	order += "<div class='order'>>."
-	order += "<span class='label><spam"
+	order += "<div class='order' id='"+data.id+"'>";
+	order += "<span class='label'>"+data.drink+"</span>";
+	order += "<span class='label'>"+data.mixer+"</span>";
+	order += "<button class='control'>Begin</button>";
+	order += "</div>";
+	ordersDiv.innerHTML += order;
+});
+
+socket.on('end', function(data){
+	//remove element at data.id
+	
 });
